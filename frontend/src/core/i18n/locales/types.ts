e357a8bf9c9601cf90d803eb4b8b73dac9f032df
Nan Gao <thunderggnn@gmail.com>
2026-05-04 03:38:05 +0200

    fix(frontend): avoid misleading error message when agent api is disable (#2697) (#2698)

--- frontend/src/core/i18n/locales/types.ts
+++ frontend/src/core/i18n/locales/types.ts
@@ -141,6 +141,7 @@ export interface Translations {
     nameStepAlreadyExistsError: string;
     nameStepNetworkError: string;
     nameStepCheckError: string;
+    nameStepApiDisabledError: string;
     nameStepBootstrapMessage: string;
     save: string;
     saving: string;
